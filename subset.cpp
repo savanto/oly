a716aeb951da38c25ce66ffdf8edf1c99749179d
savanto <savanto@gmail.com>
2013-12-06 12:45:11 -0800

    ...

--- subset.cpp
+++ subset.cpp
@@ -11,6 +11,31 @@ using namespace std;
 #define INFILE "subset.in"
 #define OUTFILE "subset.out"
 
+void subsets(int p[], int P, int s[], int S)
+{
+	if (S == 0)
+	{
+		if (P == 0)
+		{
+			cout << "  null" << endl << '}' << endl;
+			return;
+		}
+		cout << "  { ";
+		for (int i = 0; i < P; i++)
+			cout << p[i] << ' ';
+		cout << '}' << endl;
+		return;
+	}
+	int j, q[P+1], t[S-1];
+	for (j = 0; j < P; j++)
+		q[j] = p[j];
+	q[P] = s[0];
+	for (j = 1; j < S; j++)
+		t[j-1] = s[j];
+	subsets(q, P + 1, t, S - 1);
+	subsets(p, P, t, S - 1);
+}
+
 int main()
 {
 	ifstream fin(INFILE);
@@ -18,11 +43,11 @@ int main()
 	fin >> N;
 	fin.close();
 
-
-
-
-
-
+	int n[N], P = 0, p[P];
+	for (int i = 1; i <= N; i++)
+		n[i-1] = i;
+	cout << '{' << endl;
+	subsets(p, P, s, S);
 
 	ofstream fout(OUTFILE);
 
