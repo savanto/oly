aede6e33b7a0bdb95081afabc83e9bf7f4b0a308
savanto <savanto@gmail.com>
2013-12-07 12:47:16 -0800

    Updated .gitignore; working on subset

--- subset.cpp
+++ subset.cpp
@@ -11,45 +11,52 @@ using namespace std;
 #define INFILE "subset.in"
 #define OUTFILE "subset.out"
 
-void subsets(int p[], int P, int s[], int S)
-{
-	if (S == 0)
-	{
-		if (P == 0)
-		{
-			cout << "  null" << endl << '}' << endl;
-			return;
-		}
-		cout << "  { ";
-		for (int i = 0; i < P; i++)
-			cout << p[i] << ' ';
-		cout << '}' << endl;
-		return;
-	}
-	int j, q[P+1], t[S-1];
-	for (j = 0; j < P; j++)
-		q[j] = p[j];
-	q[P] = s[0];
-	for (j = 1; j < S; j++)
-		t[j-1] = s[j];
-	subsets(q, P + 1, t, S - 1);
-	subsets(p, P, t, S - 1);
-}
-
 int main()
 {
 	ifstream fin(INFILE);
 	int N;
 	fin >> N;
 	fin.close();
-
-	int n[N], P = 0, p[P];
-	for (int i = 1; i <= N; i++)
-		n[i-1] = i;
-	cout << '{' << endl;
-	subsets(p, P, s, S);
-
 	ofstream fout(OUTFILE);
+
+	int W = 0;
+	for (int i = 1; i <= N; ++i)
+		W += i;
+
+	// Sum must be even, else it is not possible to make subsets of equal sums
+	if (W % 2 != 0)
+		fout << 0 << endl;
+	else	// Variation of the Knapsack problem
+	{
+		W /= 2;	// this is the capacity of the knapsack
+		int s[N+1][W+1], i, j;
+		for (i = 0, j = 0; j <= W; ++j)
+			s[i][j] = 0;
+		for (i = 1; i <= N; ++i)
+		{
+			s[i][0] = 0;
+			for (j = 1; j <= W; ++j)
+			{
+				if (s[i-1][j])
+					s[i][j] = 1;
+				else if (s[i-1][j-i])
+					s[i][j] = 1;
+				else
+					s[i][j] = 0;
+			}
+		}
+
+		for (i = 0; i <= N; ++i)
+		{
+			for (j = 0; j <= W; ++j)
+				cout << s[i][j] << ' ';
+			cout << endl;
+		}
+	}
+
+
+
+
 
 	fout.close();
 	return 0;
