a261c4671755e3e1644fd283f87a62c6cdd39e50
savanto <savanto@gmail.com>
2013-12-08 13:41:47 -0800

    working on subset

--- subset.cpp
+++ subset.cpp
@@ -11,9 +11,18 @@ using namespace std;
 #define INFILE "subset.in"
 #define OUTFILE "subset.out"
 
-int max(int a, int b)
+int count(int **v, int i, int w)
 {
-	return (a > b ? a : b);
+	if (w == 0 || w == i * (i + 1) / 2)
+		return 1;
+	if (w < 0 || i < 1 || w > i * (i + 1) / 2)
+		return 0;
+	if (v[i][w])
+		return v[i][w];
+	int c = count(v, w, i - 1);
+	c += count(v, w - i, i - 1);
+	v[i][w] = c;
+	return c;
 }
 
 int main()
@@ -33,42 +42,35 @@ int main()
 		fout << 0 << endl;
 	else	// Variation of the Knapsack problem
 	{
-		W /= 2;	// this is the capacity of the knapsack
+		W /= 2;	// 1/2 of total sequence sum is the capacity of the knapsack
 		int v[N+1][W+1], i, w, s = 0;
-		for (w = 0; w <= W; ++w)
-			v[0][w] = 0;
 		for (i = 0; i <= N; ++i)
-			v[i][0] = 0;
-		for (w = 1; w <= W; ++w)
+			for (w = 0; w <= W; ++w)
+				v[i][w] = 0;
+/*
+		for (i = 1; i <= N; ++i)
 		{
-			for (i = 0; i <= N; ++i)
+			for (w = 0; w <= W; ++w)
 			{
 				if (i <= w)
 				{
-					if (i + v[i-1][w-i] >= v[i-1][w])
-					{
-						v[i][w] = i + v[i-1][w-i];
-						cout << i + v[i-1][w-i] << ">=" <<v[i-1][w] << endl;
-					}
-					else
-					{
-						v[i][w] = v[i-1][w];
-					}
+					if (v[i-1][w])
+						++v[i][w];
+					if (v[i-1][w-i])
+						++v[i][w];
 				}
 				else
-				{
-					v[i][w] = v[i-1][w];
-				}
+					++v[i][w];
 			}
 		}
-
-		for (i = 0; i <= N; ++i)
+*/
+		cout << count(v, N, W) << endl;
+		for (i = 1; i <= N; ++i)
 		{
 			for (w = 0; w <= W; ++w)
 				cout << v[i][w] << ' ';
 			cout << endl;
 		}
-		cout << s << endl;
 	}
 
 
