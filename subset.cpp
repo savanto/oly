f2bb2141bafd831b60e3b4fc2ce4ba0a55f98e62
savanto <savanto@gmail.com>
2013-12-08 14:12:35 -0800

    finished subset

--- subset.cpp
+++ subset.cpp
@@ -11,71 +11,30 @@ using namespace std;
 #define INFILE "subset.in"
 #define OUTFILE "subset.out"
 
-int count(int **v, int i, int w)
-{
-	if (w == 0 || w == i * (i + 1) / 2)
-		return 1;
-	if (w < 0 || i < 1 || w > i * (i + 1) / 2)
-		return 0;
-	if (v[i][w])
-		return v[i][w];
-	int c = count(v, w, i - 1);
-	c += count(v, w - i, i - 1);
-	v[i][w] = c;
-	return c;
-}
-
 int main()
 {
 	ifstream fin(INFILE);
 	int N;
 	fin >> N;
 	fin.close();
+
 	ofstream fout(OUTFILE);
 
-	int W = 0;
-	for (int i = 1; i <= N; ++i)
-		W += i;
-
-	// Sum must be even, else it is not possible to make subsets of equal sums
+	int W = N * (N + 1) / 2;
 	if (W % 2 != 0)
 		fout << 0 << endl;
-	else	// Variation of the Knapsack problem
+	else
 	{
-		W /= 2;	// 1/2 of total sequence sum is the capacity of the knapsack
-		int v[N+1][W+1], i, w, s = 0;
-		for (i = 0; i <= N; ++i)
-			for (w = 0; w <= W; ++w)
-				v[i][w] = 0;
-/*
-		for (i = 1; i <= N; ++i)
-		{
-			for (w = 0; w <= W; ++w)
-			{
-				if (i <= w)
-				{
-					if (v[i-1][w])
-						++v[i][w];
-					if (v[i-1][w-i])
-						++v[i][w];
-				}
-				else
-					++v[i][w];
-			}
-		}
-*/
-		cout << count(v, N, W) << endl;
-		for (i = 1; i <= N; ++i)
-		{
-			for (w = 0; w <= W; ++w)
-				cout << v[i][w] << ' ';
-			cout << endl;
-		}
+		W /= 2;
+		long long s[W * W];
+		s[0] = 1;
+		for (int i = 1; i < W * W; ++i)
+			s[i] = 0;
+		for (int i = 1; i <= N; ++i)
+			for (int w = W - i; w >= 0; --w)
+				s[w + i] += s[w];
+		fout << s[W] / 2 << endl;
 	}
-
-
-
-
 
 	fout.close();
 	return 0;
