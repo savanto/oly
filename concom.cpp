1a1340d1d807a43e14fbd9e2900589ab47ca320d
savanto <savanto@gmail.com>
2013-12-08 18:33:32 -0800

    working on concom

--- concom.cpp
+++ concom.cpp
@@ -14,10 +14,38 @@ using namespace std;
 int main()
 {
 	ifstream fin(INFILE);
-
+	int N;
+	fin >> N;
+	int control[N][N], p, c;
+	for (p = 0; p < N; ++p)
+		for (c = 0; c < N; ++c)
+			control[p][c] = 0;
+	for (int i = 0; i < N; ++i)
+	{
+		int stake;
+		fin >> p >> c >> stake;
+		control[p-1][c-1] = stake;
+	}
 	fin.close();
 
+	for (p = 0; p < N; ++p)
+	{
+		for (c = 0; c < N; ++c)
+		{
+			for (int cp = 0; cp < N; ++cp)
+			{
+				if (control[p][cp] > 50)
+					control[p][c] += control[cp][c];
+			}
+		}
+	}
+
 	ofstream fout(OUTFILE);
+
+	for (p = 0; p < N; ++p)
+		for (c = 0; c < N; ++c)
+			if (control[p][c] > 50)
+				fout << p+1 << ' ' << c+1 << endl;
 
 	fout.close();
 	return 0;
